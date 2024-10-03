fd733b36f635d48d764376683a3233534195ac1a
Nielvl <97364595+Nielvl@users.noreply.github.com>
2024-10-03 06:03:07 +0200

    clean start

---
+++ customerData.js
@@ -0,0 +1,419 @@
+const customerData = {
+  "customers": [
+      {
+          "id": "thiry-gent",
+          "name": "THIRY GENT",
+          "customerNumber": "400616",
+          "timeSlot": "06:00",
+          "voorkeuren": "warehouse nr 1, it's a transporting company so the truck will change from time to time.",
+          "voertuigFoto": "",
+          "locatie": "50.87836494079864, 4.369754586022119"
+      },
+      {
+        "id": "servais-alex",
+        "name": "SERVAIS ALEX",
+        "customerNumber": "4000957",
+        "timeSlot": "04:30",
+        "voorkeuren": "THE TRUCK SAYS 'LA PETITE ECHOPPE' ON THE SIDE",
+        "voertuigFoto": "",
+        "locatie": "50.87533413237001, 4.371306783271611"
+      },
+      {
+        "id": "wyla",
+        "name": "WYLA",
+        "customerNumber": "4000842",
+        "timeSlot": "05:30",
+        "voorkeuren": "WHITE TRUCK WITH A PLATFORM",
+        "voertuigFoto": "",
+        "locatie": "50.87687482582162, 4.369163161645167"
+      },
+      {
+        "id": "verdoodt-frans",
+        "name": "VERDOODT FRANS bv",
+        "customerNumber": "400779",
+        "timeSlot": "",
+        "voorkeuren": "WILL COLLECT HIMSELF",
+        "voertuigFoto": "",
+        "locatie": ""
+      },
+      {
+        "id": "lahaye-lardies",
+        "name": "LAHAYE LARDIES SPRL",
+        "customerNumber": "401000",
+        "timeSlot": "",
+        "voorkeuren": "WILL COLLECT HIMSELF",
+        "voertuigFoto": "",
+        "locatie": ""
+      },
+      {
+        "id": "gokce",
+        "name": "GOKCE",
+        "customerNumber": "400759",
+        "timeSlot": "",
+        "voorkeuren": "WILL COLLECT HIMSELF",
+        "voertuigFoto": "",
+        "locatie": ""
+      },
+      {
+        "id": "vemacro",
+        "name": "VEMACRO BV",
+        "customerNumber": "4000187",
+        "timeSlot": "",
+        "voorkeuren": "PREFERS TO BE DELIVERED AT M57",
+        "voertuigFoto": "",
+        "locatie": "50.87597895833556, 4.370438667853808"
+      },
+      {
+        "id": "de-smet-jean",
+        "name": "DE SMET JEAN bv",
+        "customerNumber": "4000078",
+        "timeSlot": "04:30",
+        "voorkeuren": "RIGHT WAREHOUSE IN THE PICTURE= C30",
+        "voertuigFoto": "https://dl.dropboxusercontent.com/scl/fi/7o90xux1dmbriqosfmk23/Foto-20-09-2024-08-17-21.jpg?rlkey=99uir53w4313xknqe29m0fbvh&st=jiapdh7x&dl",
+        "locatie": "50.87422376311202, 4.366673280546562"
+      },
+      {
+        "id": "gfresh-italian-food",
+        "name": "GFRESH ITALIAN FOOD",
+        "customerNumber": "4000375",
+        "timeSlot": "",
+        "voorkeuren": "WILL COLLECT HIMSELF",
+        "voertuigFoto": "",
+        "locatie": ""
+      },
+      {
+        "id": "van-dijck-freres",
+        "name": "VAN DIJCK FRERES NV",
+        "customerNumber": "400569",
+        "timeSlot": "06:30",
+        "voorkeuren": "COULD BE A VAN OR A TRUCK, WHEN IT'S A VAN THEY NEED IT QUICK, THE TRUCK WILL LEAVE AROUND 07:30 +/-",
+        "voertuigFoto": "",
+        "locatie": "50.87677800229558, 4.371743796279549"
+      },
+      {
+        "id": "ad-global",
+        "name": "AD-GLOBAL (oud DANFRUITS)",
+        "customerNumber": "400770",
+        "timeSlot": "",
+        "voorkeuren": "WILL COLLECT HIMSELF",
+        "voertuigFoto": "",
+        "locatie": ""
+      },
+      {
+        "id": "hoes",
+        "name": "HOES FRUITS sa",
+        "customerNumber": "400643",
+        "timeSlot": "08:30",
+        "voorkeuren": "Could order extra stuff until 08:00",
+        "voertuigFoto": "https://dl.dropboxusercontent.com/scl/fi/f1p6z6cfwx4pk8dk4h8de/Foto-20-09-2024-08-41-02.jpg?rlkey=gljf49t3x95wh46672uagotda&st=m3w02yzo&dl",
+        "locatie": "51.2240454, 4.4167634"
+      },
+      {
+        "id": "allgro",
+        "name": "ALLGRO",
+        "customerNumber": "400815",
+        "timeSlot": "09:00",
+        "voorkeuren": "WILL LOAD AT SUPERFRUIT. THE DRIVER WILL ANNOUNCE HIMSELF WHEN HE'S READY TO LOAD",
+        "voertuigFoto": "",
+        "locatie": "50.87786331412451, 4.368525418151219"
+      },
+      {
+        "id": "allfruits",
+        "name": "ALLFRUITS",
+        "customerNumber": "4000278",
+        "timeSlot": "05:00",
+        "voorkeuren": "IS PARKED IN FRONT OF COOL",
+        "voertuigFoto": "",
+        "locatie": "50.875690893824704, 4.37050526124101"
+      },
+      {
+        "id": "jacques-remy",
+        "name": "JACQUES REMY & FILS SA",
+        "customerNumber": "400342",
+        "timeSlot": "07:00 - 08:00",
+        "voorkeuren": "",
+        "voertuigFoto": "",
+        "locatie": "50.87814991581628, 4.370361855333375"
+      },
+      {
+        "id": "vandeweghe-stieven",
+        "name": "VANDEWEGHE STIEVEN",
+        "customerNumber": "4000979",
+        "timeSlot": "08:00",
+        "voorkeuren": "",
+        "voertuigFoto": "",
+        "locatie": "50.877795245567484, 4.370730212366253"
+      },
+      {
+        "id": "desaeger",
+        "name": "DESAEGER BV",
+        "customerNumber": "400876",
+        "timeSlot": "",
+        "voorkeuren": "",
+        "voertuigFoto": "",
+        "locatie": ""
+      },
+      {
+        "id": "fresh-frozen-food",
+        "name": "FRESH FROZEN FOOD",
+        "customerNumber": "4000462",
+        "timeSlot": "",
+        "voorkeuren": "",
+        "voertuigFoto": "",
+        "locatie": ""
+      },
+      {
+        "id": "delidis",
+        "name": "DELIDIS",
+        "customerNumber": "4000122",
+        "timeSlot": "08:00",
+        "voorkeuren": "Either they have their own truck or we deliver it at Andiprim, in general it is written on the delivery note. It can be combined with Delidis",
+        "voertuigFoto": "https://www.imageneuf.be/onewebmedia/Camion%20Starfresh.jpg?etag=%225f083-5c5e9592%22&sourceContentType=image%2Fjpeg",
+        "locatie": "50.877831976525336, 4.370717231621229"
+      },
+      {
+        "id": "vanlommel-fresh-food",
+        "name": "VANLOMMEL FRESH FOOD bv",
+        "customerNumber": "400059",
+        "timeSlot": "",
+        "voorkeuren": "",
+        "voertuigFoto": "",
+        "locatie": ""
+      },
+      {
+        "id": "fa-way-sa",
+        "name": "FA-WAY SA",
+        "customerNumber": "4000912",
+        "timeSlot": "",
+        "voorkeuren": "",
+        "voertuigFoto": "",
+        "locatie": ""
+      },
+      {
+        "id": "la-provencale-sarl",
+        "name": "LA PROVENCALE SARL",
+        "customerNumber": "400950",
+        "timeSlot": "",
+        "voorkeuren": "",
+        "voertuigFoto": "",
+        "locatie": ""
+      },
+      {
+        "id": "duva",
+        "name": "DUVA FRUIT",
+        "customerNumber": "400017",
+        "timeSlot": "07:00",
+        "voorkeuren": "Generally will load himseld but there is a possibility he wants to load with JM trans.",
+        "voertuigFoto": "",
+        "locatie": "50.87648084669674, 4.372139229885923"
+      },
+      {
+        "id": "horeca-totaal-brugge",
+        "name": "HORECA TOTAAL BRUGGE BV",
+        "customerNumber": "4000132",
+        "timeSlot": "",
+        "voorkeuren": "",
+        "voertuigFoto": "",
+        "locatie": ""
+      },
+      {
+        "id": "wouters-willy-en-co",
+        "name": "WOUTERS Willy en Co",
+        "customerNumber": "400699",
+        "timeSlot": "",
+        "voorkeuren": "BETWEEN HALL F AND E",
+        "voertuigFoto": "https://dl.dropboxusercontent.com/scl/fi/lzye1cwza8eg2cbzjrba1/Foto-20-09-2024-08-19-06.jpg?rlkey=iqsv2cphaku96uh3duf0qvhl1&st=tico27my&dl",
+        "locatie": "50.875344706078735, 4.367802424045308"
+      },
+      {
+        "id": "de-fruittoren-bv",
+        "name": "DE FRUITTOREN bv",
+        "customerNumber": "400619",
+        "timeSlot": "",
+        "voorkeuren": "WILL PICK UP HIMSELF",
+        "voertuigFoto": "",
+        "locatie": ""
+      },
+      {
+        "id": "star-fresh-liege",
+        "name": "STARFRESH Liege SA",
+        "customerNumber": "400686",
+        "timeSlot": "08:00",
+        "voorkeuren": "Thursday: own truck, other days it needs to be delivered at Andiprim and can be combined with Star Fresh Liége",
+        "voertuigFoto1": "https://andiprim.be/wp-content/uploads/2017/04/Fuhrpark-Andiprim-2014-08-20-001-B-de-kopie-1-w3000.jpg",
+        "voertuigFoto2": "https://dl.dropboxusercontent.com/scl/fi/r3tuq36389szwrsatq6tp/Foto-20-09-2024-07-12-42.jpg?rlkey=06dh36aa2qso49c308ott3dhh&st=olwocbe8&dl",
+        "locatie": "50.877831976525336, 4.370717231621229"
+      },
+      {
+        "id": "de-winne-guy",
+        "name": "DE WINNE GUY NV Fruit en Groenten",
+        "customerNumber": "400649",
+        "timeSlot": "05:30",
+        "voorkeuren": "The truck looks like an ex 'ringoot' truck, white and blue.",
+        "voertuigFoto": "",
+        "locatie": "50.877286606244205, 4.371222838032669"
+      },
+      {
+        "id": "verdigro",
+        "name": "VERDIGRO BV",
+        "customerNumber": "4000411",
+        "timeSlot": "06:00",
+        "voorkeuren": "Van Lier OR Could have his own truck. IF YOU NEED TO DROP IT OFF AT VAN LIER THEN ALWAYS ANNOUNCE IT IN THE SMALL OFFICE",
+        "voertuigFoto": "",
+        "locatie": "50.87788295752422, 4.370364990537037"
+      },
+      {
+        "id": "eurofruits-marcinelle",
+        "name": "EUROFRUITS Marcinelle",
+        "customerNumber": "400253",
+        "timeSlot": "",
+        "voorkeuren": "WHEN ASKED TO BE DELIVERED AT AIDA THEN MAKE SURE IT IS BEFORE 04:00",
+        "voertuigFoto": "",
+        "locatie": "50.87779783451739, 4.370724335522305"
+      },
+      {
+        "id": "restofrais",
+        "name": "RESTOFRAIS",
+        "customerNumber": "4000018",
+        "timeSlot": "05:30 - 06:00",
+        "voorkeuren": "",
+        "voertuigFoto": "",
+        "locatie": "50.87841375413854, 4.370329582359883"
+      },
+      {
+        "id": "vanderpooten-primeurs-nv",
+        "name": "VANDERPOOTEN PRIMEURS nv (ex BOLLAERTS)",
+        "customerNumber": "400819",
+        "timeSlot": "",
+        "voorkeuren": "HALL D",
+        "voertuigFoto": "https://dl.dropboxusercontent.com/scl/fi/hbw4jj52pz656g1oeprwd/VPTN.jpg?rlkey=4zmwx55nfx8x284u7mhij7rp9&st=ki2ckkrg&dl",
+        "locatie": "50.87395079777418, 4.368262318993959"
+      },
+      {
+        "id": "vlaemynck",
+        "name": "VLAEMYNCK",
+        "customerNumber": "4000200",
+        "timeSlot": "",
+        "voorkeuren": "USES A TRANSPORT COMPANY SO WILL PROBABLY CHANGE FROM TIME TO TIME. ONLY DELIVER WHEN WE SAY YOU ARE ALLOWED TO DO SO.",
+        "voertuigFoto": "",
+        "locatie": "50.87838663226401, 4.3702672701005145"
+      },
+      {
+        "id": "tirtiaux-fruits-srl",
+        "name": "TIRTIAUX & FRUITS SRL (DELHAISE LES JUMEAUX)",
+        "customerNumber": "400866",
+        "timeSlot": "",
+        "voorkeuren": "WILL PICK UP HIMSELF",
+        "voertuigFoto": "",
+        "locatie": ""
+      },
+      {
+        "id": "van-den-eede-nv",
+        "name": "VAN DEN EEDE nv",
+        "customerNumber": "400941",
+        "timeSlot": "",
+        "voorkeuren": "WILL PICK UP HIMSELF",
+        "voertuigFoto": "",
+        "locatie": ""
+      },
+      {
+        "id": "arda-can",
+        "name": "ARDA CAN",
+        "customerNumber": "4000637",
+        "timeSlot": "",
+        "voorkeuren": "",
+        "voertuigFoto": "",
+        "locatie": "50.87722842635207, 4.371240658126023"
+      },
+      {
+        "id": "tsas",
+        "name": "T'SAS",
+        "customerNumber": "4000534",
+        "timeSlot": "",
+        "voorkeuren": "WILL PICK UP HIMSELF",
+        "voertuigFoto": "",
+        "locatie": ""
+      },
+      {
+        "id": "vanderschrick-kwekerijverpakking-bv",
+        "name": "VANDERSCHRICK Kwekerijverpakking bv",
+        "customerNumber": "4000250",
+        "timeSlot": "",
+        "voorkeuren": "WILL PICK UP HIMSELF",
+        "voertuigFoto": "",
+        "locatie": ""
+      },
+      {
+        "id": "foodexpress-nc-bv",
+        "name": "FOODEXPRESS NC BV",
+        "customerNumber": "4000070",
+        "timeSlot": "",
+        "voorkeuren": "WILL PICK UP HIMSELF",
+        "voertuigFoto": "",
+        "locatie": ""
+      },
+      {
+        "id": "rodiers-nv",
+        "name": "RODIERS NV",
+        "customerNumber": "400445",
+        "timeSlot": "",
+        "voorkeuren": "",
+        "voertuigFoto": "",
+        "locatie": "50.877077031510964, 4.371443679384686"
+      },
+      {
+        "id": "fresh-spegelaere",
+        "name": "FRESH SPEGELAERE n.v.",
+        "customerNumber": "400784",
+        "timeSlot": "06:30-07:00",
+        "voorkeuren": "JM trans, in front of Van Lier",
+        "voertuigFoto": "https://dl.dropboxusercontent.com/scl/fi/v194i3usrh1efrt31p231/Foto-20-09-2024-07-26-21.jpg?rlkey=07qlqdm7qpqo297p34ztpsvbx&st=evvm03l9&dl",
+        "locatie": "50.87788295752422, 4.370364990537037"
+      },
+      {
+        "id": "vds-food-wavre",
+        "name": "VDS FOOD Wavre",
+        "customerNumber": "4000204",
+        "timeSlot": "",
+        "voorkeuren": "WILL PICK UP HIMSELF AND WILL PARK AT FRUDICOM",
+        "voertuigFoto": "",
+        "locatie": ""
+      },
+      {
+        "id": "vds-food-nv-bxl",
+        "name": "VDS Food NV BXL",
+        "customerNumber": "4000074",
+        "timeSlot": "",
+        "voorkeuren": "WILL PICK UP HIMSELF AND WILL PARK AT FRUDICOM",
+        "voertuigFoto": "",
+        "locatie": ""
+      },
+      {
+        "id": "fast-and-fresh",
+        "name": "FAST and FRESH",
+        "customerNumber": "4000276",
+        "timeSlot": "06:00",
+        "voorkeuren": "WAREHOUSE VAN LIER BUT ON THE OTHER SIDE OF THE BUILDING. Deliver it together with Bouvry between 05:30 and 06:00.",
+        "voertuigFoto": "",
+        "locatie": "50.87744851368797, 4.368868283511946"
+      },
+      {
+        "id": "de-naeghel",
+        "name": "DE NAEGHEL",
+        "customerNumber": "400623",
+        "timeSlot": "",
+        "voorkeuren": "",
+        "voertuigFoto": "",
+        "locatie": "50.87560192445967, 4.371706648734485"
+      },
+      {
+        "id": "carrefour-belgium-nv",
+        "name": "CARREFOUR BELGIUM NV",
+        "customerNumber": "400406",
+        "timeSlot": "",
+        "voorkeuren": "",
+        "voertuigFoto": "",
+        "locatie": ""
+      }
+    ]
+  }
